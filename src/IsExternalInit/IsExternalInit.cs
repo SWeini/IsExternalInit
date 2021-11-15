8a9b9cf9046f240fa0001d719c19da40957fb795
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-11-15 17:39:44 +0100

    Added conditional .NET compiler constant.

--- src/IsExternalInit/IsExternalInit.cs
+++ src/IsExternalInit/IsExternalInit.cs
@@ -14,19 +14,19 @@
 
 #region License
 // MIT License
-// 
+//
 // Copyright (c) Manuel Römer
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,7 +36,7 @@
 // SOFTWARE.
 #endregion
 
-#if !ISEXTERNALINIT_DISABLE
+#if !NET && !ISEXTERNALINIT_DISABLE
 #nullable enable
 #pragma warning disable
 
@@ -63,4 +63,4 @@ namespace System.Runtime.CompilerServices
 
 #pragma warning restore
 #nullable restore
-#endif // ISEXTERNALINIT_DISABLE
+#endif // !NET && ISEXTERNALINIT_DISABLE
